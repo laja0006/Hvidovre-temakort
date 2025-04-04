900b251ec0c0b5dade80e986119e438d39babad1
laja0006 <ljakobsen93@gmail.com>
2025-04-04 16:59:07 +0200

    Add files via upload

---
+++ styles/Gruppe7temakortHospital_5_style.js
@@ -0,0 +1,38 @@
+var size = 0;
+var placement = 'point';
+
+var style_Gruppe7temakortHospital_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#fe001e";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Hospitals") !== null) {
+        labelText = String(feature.get("Hospitals"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [580, 580],
+                  scale: 0.032758620689655175,
+                  anchor: [290.0, 290.0],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/health_hospital.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ styles/Gruppe7temakortVej_1_style.js
@@ -0,0 +1,48 @@
+var size = 0;
+var placement = 'point';
+function categories_Gruppe7temakortVej_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(194,194,194,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'red':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Gruppe7temakortVej_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("colour");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_Gruppe7temakortVej_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
